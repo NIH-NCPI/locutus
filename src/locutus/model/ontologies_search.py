a5f017ac599df312b61f67c9e90bf1fb07e13de9
Brenda <gutmanb@VAWDKG4Q04>
2024-08-05 11:51:35 -0400

    Edit ontologies model

--- src/locutus/model/ontologies_search.py
+++ src/locutus/model/ontologies_search.py
@@ -8,16 +8,16 @@ class Ontologies:
     Represents an individual ontology with various attributes.
 
     Attributes:
-        code (str): A unique code for the ontology.
-        title (str): The title of the ontology.
+        ontology_code (str): A unique code for the ontology.
+        ontology_title (str): The title of the ontology.
         system (str): The system URL associated with the ontology.
         curie (str: optional): The CURIE (Compact URI) for the ontology.
         version (str: optional): The version of the ontology.
     """
 
-    def __init__(self, code, title, system, curie="",  version=""):
-        self.code = code
-        self.title = title
+    def __init__(self, ontology_code, ontology_title, system, curie="",  version=""):
+        self.ontology_code = ontology_code
+        self.ontology_title = ontology_title
         self.system = system
         self.curie = curie
         self.version = version
@@ -26,10 +26,10 @@ class Ontologies:
         """
         Marshmallow schema for serializing and deserializing Ontologies instances.
         """        
-        code = fields.Str(
+        ontology_code = fields.Str(
             required=True, error_messages={"required": "Ontologies *must* have a code "}
         )
-        title = fields.Str(
+        ontology_title = fields.Str(
             required=True, error_messages={"required": "Ontologies *must* have a title "}
         )
         system = fields.URL(
@@ -58,35 +58,33 @@ class OntologyAPI(Serializable):
 
     Attributes:
         id (str): Unique identifier for the API.
-        name (str): Name of the API.
-        url (str): The URL endpoint for the API.
+        api_source (str): Source of the API.
+        api_url (str): The URL endpoint for the API.
         ontologies (list): A list of Ontologies objects.
-        resource_type (str): The resource type (default is "OntologyAPI").
+        resource_type (str): The resource type.
     """
     
     def __init__(
         self,
         id=None,
-        name=None,
-        url=None,
+        api_source=None,
+        api_url=None,
         ontologies=None,
         resource_type="OntologyAPI",
 
     ):
         super().__init__(id=id, collection_type="OntologyAPI", resource_type=resource_type)
         self.id = id
-        self.name = name
-        self.url = url
+        self.api_source = api_source
+        self.api_url = api_url
         self.ontologies = []
 
         if ontologies is not None:
             for ontology in ontologies:
                 if type(ontology) is dict:
                     ontology = Ontologies(**ontology)
-                # print(ontology)
-                ontology.system = self.url
+                ontology.system = self.api_url
                 self.ontologies.append(ontology)
-
 
         # Generate an ID if not provided
         super().identify()
@@ -128,9 +126,9 @@ class OntologyAPI(Serializable):
         Marshmallow schema for serializing and deserializing OntologyAPI instances.
         """
         id = fields.Str()
-        name = fields.Str(required=True)
-        url = fields.Url(required=True)
-        ontologies = fields.List(fields.Nested(Ontologies._Schema))
+        api_source = fields.Str(required=True)
+        api_url = fields.Url(required=True)
+        ontologies = fields.List(fields.Nested(Ontologies.OntologiesSchema))
 
 
         @post_load
