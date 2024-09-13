f19766475824f32d837d1ecc7c43d569144fa210
brendagutman <brenda.gutman@gmail.com>
2024-09-13 09:56:50 -0500

    [FD-1275] Add cookie security

--- src/app.py
+++ src/app.py
@@ -150,4 +150,3 @@ def not_found(e):
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
-    
--- src/locutus/model/sessions.py
+++ src/locutus/model/sessions.py
@@ -1,21 +1,34 @@
 from flask import session
+from flask_session import Session
+
+import secrets
 from datetime import timedelta
 
 class SessionManager:
     def __init__(self, app):
         self.app = app
+        # Sessions will persist beyond browser close
         self.app.config['SESSION_PERMANENT'] = True
+
+        # Generates a secure 32-character hex key to encrypt session data
+        self.app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+        # Store session info on server filesystem
+        self.app.config['SESSION_TYPE'] = 'filesystem'
+
+        # Extra security
+        self.app.config['SESSION_COOKIE_HTTPONLY'] = True
+        self.app.config['SESSION_COOKIE_SECURE'] = True
+        self.app.config['SESSION_COOKIE_SAMESITE'] = 'Lax' # Option: 'Strict'
+
+        Session(self.app)
 
     def initiate_session(self, user_id, affiliation=None):
         if not affiliation:
             affiliation = 'basic'
         
         session['user_id'] = user_id
-
-        if affiliation:
-            session['affiliation'] = affiliation
-        else:
-            session['affiliation'] = 'Not affiliated'
+        session['affiliation'] = affiliation
 
         # Adjust session timeout based on affiliation.
         self.set_timeout_based_on_affiliation(affiliation)
@@ -44,8 +57,8 @@ class SessionManager:
         if 'user_id' in session:
             return {
                 "message": "Session active", 
-                "user_id": session['user_id'], 
-                "affiliation": session['affiliation']
+                "user_id": session.get('user_id'), 
+                "affiliation": session.get('affiliation')
             }, 200
         else:
             return {"message": "No active session"}, 404
