3c7818876c9c09a720004054bd38cf152bd30005
brendagutman <brenda.gutman@gmail.com>
2024-12-12 12:27:06 -0600

    remove add_code error

--- src/locutus/model/terminology.py
+++ src/locutus/model/terminology.py
@@ -160,10 +160,6 @@ class Terminology(Serializable):
         return codes
 
     def add_code(self, code, display, description=None, editor=None):
-        for cc in self.codes:
-            if cc.code == code:
-                pass
-                # raise CodeAlreadyPresent(code, self.id, cc)
 
         new_coding = Coding(
             code=code, display=display, system=self.url, description=description
@@ -185,7 +181,6 @@ class Terminology(Serializable):
                 editor=editor,
                 target=code,
             )
-
 
     def remove_code(self, code, editor):
         code_found = False
