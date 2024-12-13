227a05a17f3144b469e8efb856cdf86aaed95a92
brendagutman <brenda.gutman@gmail.com>
2024-12-12 18:48:04 -0600

    put back the try except

--- src/locutus/model/terminology.py
+++ src/locutus/model/terminology.py
@@ -160,6 +160,9 @@ class Terminology(Serializable):
         return codes
 
     def add_code(self, code, display, description=None, editor=None):
+        for cc in self.codes:
+            if cc.code == code:
+                raise CodeAlreadyPresent(code, self.id, cc)
         new_coding = Coding(
             code=code, display=display, system=self.url, description=description
         )
