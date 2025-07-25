415585993a567ca61a83d5d16f3b32e33a61826b
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-07-25 13:04:20 -0500

    Added tests for relationship mappings

--- src/locutus/tests/test_terminology.py
+++ src/locutus/tests/test_terminology.py
@@ -1,6 +1,7 @@
 import pytest
 import json
 from locutus.model.terminology import Terminology, Coding, CodingMapping
+from locutus.model.exceptions import InvalidValueError
 
 import pdb
 
@@ -107,3 +108,45 @@ def test_delete_mappings(sample_terminology):
     assert len(mappings) == 0 # C2 mapping for editorA should be gone
     mappings = [mp for mp in sample_terminology.mappings("C1")["C1"] if mp.valid]
     assert len(mappings) == 0 # editorB's mapping for C1 should remain
+
+def test_mapping_relationship(sample_terminology):
+    try:
+        mapping_c1_editorA = CodingMapping("MAP_C1_A", "Map C1 A", "http://map.com/A", mapping_relationship='equivalent')
+        mapping_c1_editorB = CodingMapping("MAP_C1_B", "Map C1 B", "http://map.com/B", mapping_relationship='source-is-narrower-than-target')
+        mapping_c2_editorA = CodingMapping("MAP_C2_A", "Map C2 A", "http://map.com/A", mapping_relationship='source-is-broader-than-target')
+        mapping_c2_editorB = CodingMapping("MAP_C2_B", "Map C2 A", "http://map.com/A", mapping_relationship='')
+    except Exception as e:
+        raise pytest.fail(f"There was a problem with acceptable mapping relationships: {e}")
+    
+    with pytest.raises(InvalidValueError) as e_info:
+        #pdb.set_trace()
+        mapping_c2_editorA = CodingMapping("MAP_C2_A", "Map C2 A", "http://map.com/A", mapping_relationship='asdf')
+
+
+def test_mappings(sample_terminology):
+    # Setup some dummy mappings for testing the stub
+    mapping_c1_editorA = CodingMapping("MAP_C1_A", "Map C1 A", "http://map.com/A", mapping_relationship='equivalent')
+    mapping_c1_editorB = CodingMapping("MAP_C1_B", "Map C1 B", "http://map.com/B", mapping_relationship='equivalent')
+    mapping_c2_editorA = CodingMapping("MAP_C2_A", "Map C2 A", "http://map.com/A", mapping_relationship='equivalent')
+
+    sample_terminology.set_mapping("C1", [mapping_c1_editorA, mapping_c1_editorB], "editorA")
+    
+    sample_terminology.set_mapping("C2", [mapping_c2_editorA], "editorA")
+
+    c1_mappings = sample_terminology.mappings("C1")["C1"]
+    assert len(c1_mappings) == 2
+    assert any(m.code == "MAP_C1_A" for m in c1_mappings)
+    assert any(m.code == "MAP_C1_B" for m in c1_mappings)
+
+    # Replace the previous mappings
+    sample_terminology.set_mapping("C1", [mapping_c1_editorB], "editorA")
+    c1_mappings = sample_terminology.mappings("C1")["C1"]
+    assert len(c1_mappings) == 1
+    assert c1_mappings[0].code == "MAP_C1_B"
+    
+    c2_mappings = sample_terminology.mappings("C2")["C2"]
+    assert len(c2_mappings) == 1
+    assert c2_mappings[0].code == "MAP_C2_A"
+
+    mappings = [mp for mp in sample_terminology.mappings("C99")["C99"] if mp.valid]
+    assert len(mappings) == 0 # No mappings for non-existent code
