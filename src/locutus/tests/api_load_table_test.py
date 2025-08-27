18deb41b213804eeca01d50f83e76128d951ca9b
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-08-27 13:49:05 -0500

    Test load table as PUT

--- src/locutus/tests/api_load_table_test.py
+++ src/locutus/tests/api_load_table_test.py
@@ -1,13 +1,17 @@
+import pytest 
 from .test_datadictionary import basic_datadictionary 
 from . import client
+
+from locutus.model.table import Table 
 from .test_terminology import ftd_concept_relationships, sample_terminology
 from .test_study import basic_study
 from .test_table import basic_table 
 
+from copy import deepcopy
+
 from locutus.model.table import Table 
 
-def test_loading_table(client, ftd_concept_relationships):
-    mini_table = {
+mini_table_body = {
         "name": "mini table",
         "url": "https://mini.table",
         "csvContents": [
@@ -48,21 +52,65 @@ def test_loading_table(client, ftd_concept_relationships):
         "editor": "unit-test"
     }
 
+
+def test_loading_table(client, ftd_concept_relationships):
+    global mini_table_body
     response = client.post("/api/LoadTable",
-                            json=mini_table, 
+                            json=mini_table_body, 
                             headers={"Content-Type": "application/json"}
                         )
     assert response.status_code == 201 
     table = response.json 
 
-    assert table['name'] == mini_table['name']
-    assert len(table['variables']) == len(mini_table['csvContents'])
+    assert table['name'] == mini_table_body['name']
+    assert len(table['variables']) == len(mini_table_body['csvContents'])
+
 
     t = Table.get(table['id'])
-    assert t.name == mini_table['name']
+    assert t.name == mini_table_body['name']
     
     term = t.terminology.dereference()
-    assert len(term.codes) == len(mini_table['csvContents'])
+    assert len(term.codes) == len(mini_table_body['csvContents'])
+
+    # cleanup 
+    term.global_id().delete()
+    term.delete(hard_delete=True)
+
+    enum = t.variables[1].enumerations.dereference()
+    enum.global_id().delete()
+    enum.delete(hard_delete=True)
+
+    t.global_id().delete()
+    t.delete(hard_delete=True)
+
+def test_loading_table_put(client, ftd_concept_relationships):
+    global mini_table_body
+
+    original_table = Table(
+        name="FTD Table 01",
+        url="http://ftd.unit.tests/basic_table/01",
+        description="Simple Test Table",
+        editor="unit-test"# ,variables=[]
+    )
+    original_table.save()
+
+    response = client.put(f"/api/LoadTable/{original_table.id}",
+                            json=mini_table_body, 
+                            headers={"Content-Type": "application/json"}
+                        )
+    assert response.status_code == 201 
+    table = response.json 
+
+
+    assert table['name'] == "FTD Table 01"
+    assert len(table['variables']) == len(mini_table_body['csvContents'])
+
+
+    t = Table.get(table['id'])
+    assert t.name == "FTD Table 01"
+    
+    term = t.terminology.dereference()
+    assert len(term.codes) == len(mini_table_body['csvContents'])
 
     # cleanup 
     term.global_id().delete()
