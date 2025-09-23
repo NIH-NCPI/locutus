30de3e1f87a73728f61172777930781c0caddaca
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-09-23 14:00:39 -0500

    Versioning (#127)
    * trying to work out dynamic versioning
    
    * Integrate github version with the app itself

--- src/locutus/api/metadata.py
+++ src/locutus/api/metadata.py
@@ -1,9 +1,10 @@
 import os
 from flask_restful import Resource
+from locutus._version import __version__
 
 class Version(Resource):
     def get(self):
         """Retrieve the VERSION environment variable. The version is set at
         deployment. """
-        version = os.getenv('VERSION', 'Version not set')
+        version = os.getenv('VERSION', __version__)
         return {'version': version}
