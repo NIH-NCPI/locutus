d3dc45dc14b1e94b19436f2c4c4f44a5e2d14316
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-10-10 14:14:18 -0500

    Bugfix

--- src/locutus/api/sideload.py
+++ src/locutus/api/sideload.py
@@ -4,9 +4,10 @@ from flask import request
 from bson import json_util 
 import json
 
+from locutus.api import default_headers, get_editor
 from locutus.utility.sideload import SetMappings
 
-Class SideLoad(Resource):
+class SideLoad(Resource):
     def post(self):
         mapping_data = request.get_json()
         editor = get_editor(body=mapping_data, editor=None)
