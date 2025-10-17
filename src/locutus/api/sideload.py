d091bf21574d3ac743f481fbaad1c40555fec72e
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-10-17 15:51:23 -0500

    Proper error return

--- src/locutus/api/sideload.py
+++ src/locutus/api/sideload.py
@@ -7,9 +7,14 @@ import json
 from locutus.api import default_headers, get_editor
 from locutus.utility.sideload import SetMappings
 
+from locutus.model.exceptions import LackingRequiredParameter
+
 class SideLoad(Resource):
     def post(self):
         mapping_data = request.get_json()
         editor = get_editor(body=mapping_data, editor=None)
 
-        return SetMappings(mapping_data['csvContents'])
+        try:
+            return SetMappings(mapping_data['csvContents'])
+        except LackingRequiredParameter as e:
+            return e.to_dict(), 400
